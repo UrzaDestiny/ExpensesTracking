d05e97cdd9de59e0e8f1440c440225d5128cdc54
Pavel Levin <urzadesti@gmail.com>
2023-12-05 16:23:41 +0200

    add template screens, fix dependencies

---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { styles } from './HomeStyles';
+
+const Home: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text>This is Home</Text>
+    </View>
+  );
+};
+
+
+export default Home;
---
+++ src/screens/Login/Login.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { styles } from './LoginStyles';
+
+const Login: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text>This is Login</Text>
+    </View>
+  );
+};
+
+
+export default Login;
