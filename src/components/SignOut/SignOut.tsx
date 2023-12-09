95b1f77395bbdbc0af9502796f41c9441f5e6572
Pavel Levin <urzadesti@gmail.com>
2023-12-09 16:40:25 +0200

    SignOut update

--- src/components/SignOut/SignOut.tsx
+++ src/components/SignOut/SignOut.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import {Text, View, TouchableOpacity} from 'react-native';
-import {styles} from './SignOutStyles';
-import {NavigationProp, useNavigation} from '@react-navigation/native';
-import {RootStackParamList} from '~/types';
+import { Text, View, TouchableOpacity } from 'react-native';
+import { styles } from './SignOutStyles';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
+import { RootStackParamList } from '~/types';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useDispatch } from 'react-redux';
+import { setInitialState } from '~/features/expenses/expensesSlice';
 
 const SignOut: React.FC = () => {
   const navigation: NavigationProp<RootStackParamList> = useNavigation();
-  
-  const handleSignOut = () => {
+  const dispatch = useDispatch();
+
+  const handleSignOut = async () => {
+    await AsyncStorage.removeItem('userName');
+    await AsyncStorage.removeItem('userLoggedIn');
+    dispatch(setInitialState());
     navigation.navigate('Login');
   };
 
--- src/features/expenses/expensesSlice.ts
+++ src/features/expenses/expensesSlice.ts
@@ -11,28 +11,7 @@ interface ExpensesState {
 }
 
 const initialState: ExpensesState = {
-  expenses: [
-    {id: '1', name: 'Shopping', amount: 50, date: '2023-12-01'},
-    {id: '2', name: 'Dining', amount: 30, date: '2023-12-02'},
-    {id: '3', name: 'Groceries', amount: 20, date: '2023-12-01'},
-    {id: '4', name: 'Utilities', amount: 40, date: '2023-12-03'},
-    {id: '5', name: 'Entertainment', amount: 25, date: '2023-12-02'},
-    {id: '6', name: 'Shopping', amount: 55, date: '2023-12-04'},
-    {id: '7', name: 'Dining', amount: 35, date: '2023-12-05'},
-    {id: '8', name: 'Groceries', amount: 15, date: '2023-12-01'},
-    {id: '9', name: 'Utilities', amount: 42, date: '2023-12-03'},
-    {id: '10', name: 'Entertainment', amount: 28, date: '2023-12-05'},
-    {id: '11', name: 'Shopping', amount: 48, date: '2023-12-02'},
-    {id: '12', name: 'Dining', amount: 32, date: '2023-12-04'},
-    {id: '13', name: 'Groceries', amount: 18, date: '2023-12-01'},
-    {id: '14', name: 'Utilities', amount: 38, date: '2023-12-03'},
-    {id: '15', name: 'Entertainment', amount: 22, date: '2023-12-04'},
-    {id: '16', name: 'Shopping', amount: 60, date: '2023-12-05'},
-    {id: '17', name: 'Dining', amount: 36, date: '2023-12-02'},
-    {id: '18', name: 'Groceries', amount: 25, date: '2023-12-04'},
-    {id: '19', name: 'Utilities', amount: 45, date: '2023-12-03'},
-    {id: '20', name: 'Entertainment', amount: 30, date: '2023-12-05'},
-  ],
+  expenses: [],
   filters: {
     title: '',
     date: '',
@@ -50,11 +29,11 @@ const expensesSlice = createSlice({
     },
     editExpense: (
       state,
-      action: PayloadAction<{ id: string; updatedExpense: Expense }>,
+      action: PayloadAction<{id: string; updatedExpense: Expense}>,
     ) => {
-      const { id, updatedExpense } = action.payload;
-      const index = state.expenses.findIndex((expense) => expense.id === id);
-    
+      const {id, updatedExpense} = action.payload;
+      const index = state.expenses.findIndex(expense => expense.id === id);
+
       if (index !== -1) {
         state.expenses[index] = updatedExpense;
       }
@@ -70,10 +49,20 @@ const expensesSlice = createSlice({
     setDateFilter: (state, action) => {
       state.filters.date = action.payload;
     },
+    setInitialState: (state) => {
+      return initialState;
+    },
   },
 });
 
-export const {addExpense, editExpense, removeExpense, setTitleFilter, setDateFilter} = expensesSlice.actions;
+export const {
+  addExpense,
+  editExpense,
+  removeExpense,
+  setTitleFilter,
+  setDateFilter,
+  setInitialState
+} = expensesSlice.actions;
 export const selectExpenses = (state: RootState) => state.expenses.expenses;
 export const selectFilters = (state: RootState) => state.expenses.filters;
 export default expensesSlice.reducer;
